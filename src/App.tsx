b1d8931884e17990a20b7be5de167112d6b425f8
ArikHorowitz <82939295+ArikHorowitz@users.noreply.github.com>
2025-07-22 11:57:21 +0000

    just

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>"It Works on StackBlitz!"</h1>
+      <h1>"It Works on Syntia"</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
